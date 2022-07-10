24ed59e8417349283ea6f6583732a5f9633d6686
Elif O. Kaynak <kaynakef@gmail.com>
2022-07-10 13:14:43 -0400

    delete is working now

--- src/main/java/test/stepdefinitions/DeleteUser_StepDef.java
+++ src/main/java/test/stepdefinitions/DeleteUser_StepDef.java
@@ -12,6 +12,7 @@ import test.utils.ConfigReader;
 import test.utils.PayloadUtils;
 
 
+import java.util.List;
 import java.util.Map;
 
 public class DeleteUser_StepDef {
@@ -19,19 +20,31 @@ public class DeleteUser_StepDef {
     Response response;
     String userId;
 
+    @Given("User sets request header and authorization for delete")
+    public void user_sets_request_header_and_authorization_for_delete() {
+        requestSpecification= RestAssured.given().accept("application/json")
+                .header("Authorization", ConfigReader.readProperty("auth"));
+    }
+    @When("User sets request body, sends a Post HTTP request and validates the status for delete")
+    public void user_sets_request_body_sends_a_post_http_request_and_validates_the_status_for_delete(DataTable dataTable) {
+        Map<String,String> requestBodyCredentials=dataTable.asMap();
+        response = requestSpecification
+                .body(PayloadUtils.getCreateUserPayload(requestBodyCredentials.get("name"), requestBodyCredentials.get("gender"), PayloadUtils.createEmail(), requestBodyCredentials.get("status")))
+                .when().post().then().statusCode(200).extract().response();
 
+    }
 
     @Then("User gets the user id from the response body")
     public void user_gets_the_user_id_from_the_response_body() {
         UsersPojo parsedResponse = response.as(UsersPojo.class);
-        userId=(String)(((Map<String, Object>)(parsedResponse.getData())).get("id"));
+        userId=(String)((((List<Map<String, Object>>)(parsedResponse.getData())).get(0)).get("id"));
 
     }
 
     @When("User sets user id and sends a Delete HTTP request and validates the status")
     public void user_sets_user_id_and_sends_a_delete_http_request_and_validates_the_status() {
         response = requestSpecification
-                .when().delete(userId).then().statusCode(200).extract().response();
+                .when().delete("https://gorest.co.in/public-api/users/"+userId).then().log().body().statusCode(200).extract().response();
 
     }
     @Then("User validates the response body {int} and {string}")
--- src/main/resources/features/deleteUser.feature
+++ src/main/resources/features/deleteUser.feature
@@ -3,13 +3,13 @@ Feature: Testing the delete User functionality
     Given Users api endpoint set
 
   Scenario: Validating the successful user deleting
-    And User sets request header and authorization
+    And User sets request header and authorization for delete
 #    first we are creating a user to delete it
-    When User sets request body, sends a Post HTTP request and validates the status
+    When User sets request body, sends a Post HTTP request and validates the status for delete
       | name   | test   |
       | gender | female |
       | status | active |
-#    here i am getting null pointer exception since this before steps are coming from CreateUser_StepDef class
+
     Then User gets the user id from the response body
     When User sets user id and sends a Delete HTTP request and validates the status
     Then User validates the response body 401 and "Authentication failed"
